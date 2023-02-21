87e1c6be30106f5b166484c95f94e0b2155438df
Akif <mehmet61875@gmail.com>
2023-02-21 22:57:22 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Container from "react-bootstrap/Container";
+import CardContainer from "./components/CardContainer";
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 style={{ textAlign: "center" }}>NBA Legends App</h1>
-      </header>
-    </div>
+    <Container className="text-center mt-4">
+      <Header />
+      <CardContainer />
+    </Container>
   );
 }
 
